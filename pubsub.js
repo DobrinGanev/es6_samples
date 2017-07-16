b13297a1e09ed203d84283b1ce64559ce14bcd22
Dobrin <dobringanev@gmail.com>
2017-07-15 22:02:16 -0600

    added pubsub

---
+++ pubsub.js
@@ -0,0 +1,41 @@
+const EventManager = () => {
+  let events = {}
+
+  const publish = (name, data) => {
+    return new Promise(function(resolve, reject) {
+      var handlers = events[name]
+      if(!!handlers === false) return
+      handlers.forEach(function(handler) {
+        handler.call(this, data)
+      })
+      resolve();
+    })
+  }
+
+  const subscribe = (name, handler) => {
+    let handlers = events[name]
+    if(!!handlers === false) {
+      handlers = events[name] = []
+    }
+    handlers.push(handler)
+  };
+
+  const unsubscribe = (name, handler) => {
+    let handlers = events[name]
+    if(!!handlers === false) return
+
+    let handlerIdx = handlers.indexOf(handler)
+    handlers.splice(handlerIdx)
+  }
+	
+	return {publish, subscribe, unsubscribe}
+}
+const sub = EventManager()
+
+sub.subscribe('dataload', (data) => {
+  console.log(data)
+});
+
+
+sub.publish('dataload', {id:'1234'})
+
